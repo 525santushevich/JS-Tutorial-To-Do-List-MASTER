cba83b427c2b40d82f7d1a100fb088f98ef0375a
525santushevich <sophiaantushevich@icloud.com>
2024-05-13 10:42:35 -0400

    first day html and js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+
+
+
+
+const dateElement = document.getElementById("date");
+
+
+
+
+const list = document.getElementById("list");
+
+
+
+
+
+const input = document.getElementById("input");
